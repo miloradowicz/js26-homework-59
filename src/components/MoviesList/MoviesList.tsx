7efa43fa4bf4b6696e30aba9274c7fb2ea8d05c8
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-10 18:25:18 +0600

    MoviesList added

---
+++ src/components/MoviesList/MoviesList.tsx
@@ -0,0 +1,30 @@
+import { FC, memo } from 'react';
+import { MovieInfo, MovieMutation } from '../../types.d';
+import MoviesItem from './MoviesItem/MoviesItem';
+
+interface MoviesListProps {
+  list: MovieInfo[];
+  onUpdate: (_0: number, _1: MovieMutation) => void;
+  onDelete: (_: number) => void;
+}
+
+const MoviesList: FC<MoviesListProps> = memo(({ list, onUpdate, onDelete }) => {
+  return (
+    <div>
+      <h3 className='mb-3'>Watch list:</h3>
+      {list.length === 0 ? (
+        <div>
+          <p>Nothing to watch</p>
+        </div>
+      ) : (
+        <ul className='list-group'>
+          {list.map((x) => (
+            <MoviesItem key={x.id} title={x.title} onUpdate={(movie: MovieMutation) => onUpdate(x.id, movie)} onDelete={() => onDelete(x.id)} />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+});
+
+export default MoviesList;
